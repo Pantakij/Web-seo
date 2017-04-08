f1f43ec9984a754f7c0f4ea9b4d9803252d9227e
Pantakij <pantakij.ch@outlook.co.th>
2017-04-08 15:57:21 +0700

    social
    add facebook comment on web page, like and share page on facebook

--- public/js/font-blink.js
+++ public/js/font-blink.js
@@ -6,9 +6,11 @@ $(document).ready(function(){
     setInterval(function(){ 
         if(trigger){
             $('[name="blink"]').css('color', 'white')
+            $('[name="blink"]').css('transform', 'rotate(-15deg)')
         }
         else{
             $('[name="blink"]').css('color', defaultColor)
+            $('[name="blink"]').css('transform', 'rotate(15deg)')
         }
         trigger = !trigger
     }, 1000)
