0ffd1029865bb64ff6516371dc3f322d48396820
Pattarasakon <pongpstw@gmail.com>
2017-04-13 20:40:02 +0700

    Add page component
    Add contact scroll button and scrollspy vertical menu

---
+++ public/js/scroll-btn.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+	window.onscroll = function() {scrollFunction()};
+
+	function scrollFunction() {
+
+	     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	        document.getElementById("fixpos-btn-line-at").style.display = "block";
+	    } else {
+	        document.getElementById("fixpos-btn-line-at").style.display = "none";
+	    }
+
+	     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	        document.getElementById("fixpos-btn-call-us").style.display = "block";
+	    } else {
+	        document.getElementById("fixpos-btn-call-us").style.display = "none";
+	    }
+	}
+
+	$('[name="scrooll-btn-top"]').click(function() {
+	    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
+	    document.documentElement.scrollTop = 0; // For IE and Firefox
+
+	    /*document.getElementById("here").scrollIntoView();*/
+	})
+
+	$('[name="fixpos-btn-line-at"]').click(function(){
+        window.location = 'https://line.me/R/ti/p/%40dboonthai';
+    })
+
+    $('[name="fixpos-btn-call-us"]').click(function(){
+        window.location = 'tel:0952206249';
+    })
+})
