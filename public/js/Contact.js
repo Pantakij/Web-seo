23c622e7bfd5cb1b5a8291307220fb51104a04e8
Pantakij <pantakij.ch@outlook.co.th>
2017-04-14 12:25:59 +0700

    change style product properties
    commit

--- public/js/Contact.js
+++ public/js/Contact.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('[name="line-at"]').click(function(){
-        window.location = 'https://line.me/R/ti/p/%40dboonthai';
+        window.location = 'https://line.me/R/ti/p/%40dboonthai'
     })
 
     $('[name="call-us"]').click(function(){
-        window.location = 'tel:0952206249';
+        window.location = 'tel:0952206249'
     })
 })
--- public/js/scroll-btn.js
+++ public/js/scroll-btn.js
@@ -1,24 +1,26 @@
 $(document).ready(function(){
-	window.onscroll = function() {scrollFunction()};
+	window.onscroll = function() {
+		scrollFunction()
+	}
 
 	function scrollFunction() {
 
 	     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-	        document.getElementById("fixpos-btn-line-at").style.display = "block";
+	        document.getElementById("fixpos-btn-line-at").style.display = "block"
 	    } else {
-	        document.getElementById("fixpos-btn-line-at").style.display = "none";
+	        document.getElementById("fixpos-btn-line-at").style.display = "none"
 	    }
 
 	     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-	        document.getElementById("fixpos-btn-call-us").style.display = "block";
+	        document.getElementById("fixpos-btn-call-us").style.display = "block"
 	    } else {
-	        document.getElementById("fixpos-btn-call-us").style.display = "none";
+	        document.getElementById("fixpos-btn-call-us").style.display = "none"
 	    }
 	}
 
 	$('[name="scrooll-btn-top"]').click(function() {
-	    document.body.scrollTop = 0; // For Chrome, Safari and Opera 
-	    document.documentElement.scrollTop = 0; // For IE and Firefox
+	    document.body.scrollTop = 0 // For Chrome, Safari and Opera 
+	    document.documentElement.scrollTop = 0 // For IE and Firefox
 
 	    /*document.getElementById("here").scrollIntoView();*/
 	})
